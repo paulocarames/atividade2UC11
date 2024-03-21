a58b76a94ebc370f0fcc0da7d5c68cc79ffa0867
paulocarames <paulocarames@gmail.com>
2024-03-21 10:19:22 -0300

    Configuração de conexão com o banco de dados

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -11,7 +11,7 @@ public class conectaDAO {
 
         try {
 
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11", "root", "12345");
 
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
