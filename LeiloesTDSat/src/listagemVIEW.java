6418bac56d621eae6f488407ad958c4e79ba5743
paulocarames <paulocarames@gmail.com>
2024-03-24 01:50:31 -0300

    Listar produtos

--- LeiloesTDSat/src/listagemVIEW.java
+++ LeiloesTDSat/src/listagemVIEW.java
@@ -1,5 +1,5 @@
 
-import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.DefaultTableModel;
 
 public class listagemVIEW extends javax.swing.JFrame {
@@ -196,7 +196,7 @@ public class listagemVIEW extends javax.swing.JFrame {
             DefaultTableModel model = (DefaultTableModel) listaProdutos.getModel();
             model.setNumRows(0);
             
-            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutos();
+            List<ProdutosDTO> listagem = produtosdao.listarProdutos();
             
             for(int i = 0; i < listagem.size(); i++){
                 model.addRow(new Object[]{
